cfedea329e818ca0d85e591fd890d33b0c4916bd
sunzhangchang <642311409@qq.com>
2023-09-28 19:42:35 +0800

    update config

--- backend/config.ts
+++ backend/config.ts
@@ -11,8 +11,56 @@ export interface TokenConfigType {
     admins: Array<string>
 }
 
-export const loadConfig = (): ConfigType =>
-    JSON.parse(Deno.readTextFileSync('./backend/cfg.json'))
+const defaultCfg: ConfigType = {
+    pass: ['过', 'pass'],
+    roles: {
+        hunter: 1,
+        seer: 1,
+        villager: 3,
+        werewolf: 3,
+        witch: 1,
+    },
+    target: 'side',
+}
 
+// todo use KV(`unstable`)
+const checkEnv = (key: string, dft?: string): boolean => {
+    if (!Deno.env.has(key) && typeof dft !== 'undefined') {
+        Deno.env.set(key, dft)
+    }
+    return Deno.env.has(key)
+}
+
+export const loadConfig = (): ConfigType => {
+    checkEnv('pass', JSON.stringify(defaultCfg.pass))
+
+    for (const key in defaultCfg.roles) {
+        if (Object.prototype.hasOwnProperty.call(defaultCfg.roles, key)) {
+            checkEnv(
+                key,
+                JSON.stringify(
+                    (defaultCfg.roles as Record<string, number>)[key],
+                ),
+            )
+        }
+    }
+
+    checkEnv('target', defaultCfg.target)
+
+    return {
+        pass: JSON.parse(Deno.env.get('pass')!),
+        roles: {
+            hunter: JSON.parse(Deno.env.get('hunter')!),
+            seer: JSON.parse(Deno.env.get('seer')!),
+            villager: JSON.parse(Deno.env.get('villager')!),
+            werewolf: JSON.parse(Deno.env.get('werewolf')!),
+            witch: JSON.parse(Deno.env.get('witch')!),
+        },
+        target: Deno.env.get('target')! as Target,
+    }
+}
+// JSON.parse(Deno.readTextFileSync('./backend/cfg.json'))
+
+// todo use KV(`unstable`)
 export const getTokens = (): TokenConfigType =>
     JSON.parse(Deno.readTextFileSync('./backend/tokens.json').toString())
